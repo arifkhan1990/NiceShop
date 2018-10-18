8d9314940bb7c36bd58d6a8ab3cb949cde8807e7
Arif Khan <arifkhanshubro@gmail.com>
2018-10-19 00:35:37 +0600

    Update nav.blade.php

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,20 +1,14 @@
 <header>
     <div class="top-nav container">
-<<<<<<< HEAD
         <div class="logo"><a href="/">Nice Shop</a></div>
         @if(!request()->is('checkout))
-=======
         <div class="logo">Laravel Ecommerce</div>
->>>>>>> 025b9d0391f1f3d8b71ad8a4331585ccc063991f
         <ul>
             <li><a href="#">Shop</a></li>
             <li><a href="#">About</a></li>
             <li><a href="#">Blog</a></li>
             <li><a href="#">Cart</a></li>
         </ul>
-<<<<<<< HEAD
         @endif
-=======
->>>>>>> 025b9d0391f1f3d8b71ad8a4331585ccc063991f
     </div> <!-- end top-nav -->
 </header>
